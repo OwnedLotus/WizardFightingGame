882c6edd7dd69f77fdfd4ce938fcbbbc69d2e365
Jonah Ringdahl <JonahRingdahl@outlook.com>
2021-04-03 20:04:11 -0400

    Jumping Mechanics

--- WizardFightingGame/Assets/Scripts/CharController.cs
+++ WizardFightingGame/Assets/Scripts/CharController.cs
@@ -6,12 +6,14 @@ public class CharController : MonoBehaviour
 {
     Rigidbody rb;
     public float speed;
-    public int jumpNum, topSpeed;
+    public int jumpMax, topSpeed;
+    private int jump = 0;
     Vector3 move;
     public Vector3 jumpForce;
     public Transform rayStart;
     RaycastHit hit;
     private float mHorizontal, mForward;
+    bool isGrounded = false;
 
     // Start is called before the first frame update
  void Awake() 
@@ -27,22 +29,59 @@ public class CharController : MonoBehaviour
 
         move = new Vector3(mHorizontal, 0, mForward);
         rb.AddForce(move);
+        
+        if (isGrounded)
+        {
+            jump = jumpMax;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && jump != 0)
+        {
+            if (!isGrounded)
+            {
+                jump--;
+            }
+
+            jumpForce = new Vector3(0f, 50f, 0f);
+            rb.AddForce(jumpForce);
+        }
     }
 
     private void Update() 
     {
         
-        Jump(hit);
+        
         
     }
 
-    private void Jump(RaycastHit hit)
+    /*          For later Flying super maybe
+    private bool Jump(RaycastHit hit)
     {
+        
         if (Physics.Raycast(rayStart.position, -transform.up, out hit, 1f))
         {
-            print("isGrounded");
+            isGrounded = true;
+        }
+
+        return isGrounded;
+    }
+    */
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            print("Grounded");
         }
     }
 
-    
+    private void OnCollisionExit(Collision other) 
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            print("Not Grounded");
+        }
+    }
 }
